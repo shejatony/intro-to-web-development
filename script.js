066edcec78090720090672327ce431edc888d845
Sheja Hugues Tony <113254150+shejatony@users.noreply.github.com>
2025-04-26 15:46:00 +0300

    the second commit
    i've the made the changes to the web

--- script.js
+++ script.js
@@ -3,8 +3,8 @@ function changeBackgroundColor(color){
     section.style.backgroundColor = color;
 }
 
-document.getElementById('intro').addEventListener('click', () => changeBackgroundColor("#2CA02C"));
+document.getElementById('intro').addEventListener('click', () => changeBackgroundColor("purple"));
 
-document.getElementById('react').addEventListener('click', () => changeBackgroundColor("#FF7F0E"));
+document.getElementById('react').addEventListener('click', () => changeBackgroundColor("green"));
 
-document.getElementById('backend').addEventListener('click', () => changeBackgroundColor("#9467BD"));
+document.getElementById('backend').addEventListener('click', () => changeBackgroundColor("blue"));
